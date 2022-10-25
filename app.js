b1d0880a42873d35f76f0583545b3b6f6c67a0f2
rociocabezai <rocio.cabezai@gmail.com>
2022-10-25 17:31:23 -0300

    Proyecto completo. Home, Login y Register.

--- app.js
+++ app.js
@@ -10,4 +10,12 @@ app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html')
-})
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+});
